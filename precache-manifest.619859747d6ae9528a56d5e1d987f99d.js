b37ae675f8fd730c10e5955a72a887301b07aea3
Dilshan <dilshanramesh81@gamil.com>
2019-04-20 02:13:26 +0530

    Updates

---
+++ precache-manifest.619859747d6ae9528a56d5e1d987f99d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c225976de611a5ce309",
+    "url": "/React-Weathe-meter/static/js/runtime~main.f0a30dce.js"
+  },
+  {
+    "revision": "dab439ce604ae7f1e1d4",
+    "url": "/React-Weathe-meter/static/js/main.20a0d117.chunk.js"
+  },
+  {
+    "revision": "8995ed65ce443e708012",
+    "url": "/React-Weathe-meter/static/js/2.f6b90c5d.chunk.js"
+  },
+  {
+    "revision": "dab439ce604ae7f1e1d4",
+    "url": "/React-Weathe-meter/static/css/main.bc9b00d0.chunk.css"
+  },
+  {
+    "revision": "9c0e76db4db420f407702b0e7cdf6a8a",
+    "url": "/React-Weathe-meter/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/React-Weathe-meter/precache-manifest.619859747d6ae9528a56d5e1d987f99d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/React-Weathe-meter/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
